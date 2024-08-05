d89d7536cd61395d2c5d953fdbd06d3939a9998d
unknown <najeemnurudeen1990@gmail.com>
2024-08-05 06:51:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('YES OKAY')
+let p = 1;
+if (p % 2 == 0){
+    console.log(`${p} is even number`)
+}else{
+    console.log(`${p} is odd number`)
+
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const genres = require('./routes/genres'); // Corrected the path
+// const Joi = require('joi');
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/vividly')
+    .then(() => console.log('Connected to my database...'))
+    .catch(err => console.error('Not connected to my database...', err));
+
+app.use(express.json());
+app.use('/api/genres', genres); // Corrected the route
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`)); // Corrected the port variable
+
+// return ;
